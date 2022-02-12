f821930085c54b43803383bed8f63968526bf370
tianchang93824 <tianchang824@outlook.com>
2022-02-12 14:29:40 +0800

    修改batch和epoch

--- models/bert.py
+++ models/bert.py
@@ -19,8 +19,8 @@ class Config(object):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   # 设备
         self.require_improvement = 1000                                 # 若超过1000batch效果还没提升，则提前结束训练
         self.num_classes = len(self.class_list)                         # 类别数
-        self.num_epochs = 5                                             # epoch数
-        self.batch_size = 16                                            # mini-batch大小
+        self.num_epochs = 30                                             # epoch数
+        self.batch_size = 200                                            # mini-batch大小
         self.pad_size = 20                                              # 每句话处理成的长度(短填长切)
         self.learning_rate = 3e-5                                       # 学习率
         self.threshold = 0.5                                            # 概率阈值
