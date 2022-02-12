f0c21d12a3bda0a8a98d87ebd045de3276edce03
tianchang93824 <tianchang824@outlook.com>
2022-02-12 15:22:46 +0800

    添加随机失活

--- models/bert.py
+++ models/bert.py
@@ -36,11 +36,13 @@ class Model(nn.Module):
         self.bert = BertModel.from_pretrained(config.bert_path)
         for param in self.bert.parameters():
             param.requires_grad = True
+        self.dropout = nn.Dropout(0.3)
         self.fc = nn.Linear(config.hidden_size, config.num_classes)
 
     def forward(self, x):
         context = x[0]  # 输入的句子
         mask = x[2]  # 对padding部分进行mask，和句子一个size，padding部分用0表示，如：[1, 1, 1, 1, 0, 0]
         _, pooled = self.bert(context, attention_mask=mask, output_all_encoded_layers=False)
+        pooled = self.dropout(pooled)
         out = self.fc(pooled)
         return out
