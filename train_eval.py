37e164127671180a89cf7e5de7ceba71a92831ad
tianchang93824 <tianchang824@outlook.com>
2022-02-14 10:01:48 +0800

    加上梯度

--- train_eval.py
+++ train_eval.py
@@ -56,45 +56,45 @@ def train(config, model,model_name, train_iter, dev_iter, test_iter):
     last_improve = 0  # 记录上次验证集loss下降的batch数
     flag = False  # 记录是否很久没有效果提升
     model.train()
-    with torch.no_grad():
-        for epoch in range(config.num_epochs):
-            print('Epoch [{}/{}]'.format(epoch + 1, config.num_epochs))
-            for i, (trains, labels) in enumerate(train_iter):
-                outputs = model(trains)
-                outputs = torch.sigmoid(outputs)
-                model.zero_grad()
-                loss = nn.BCELoss()(outputs, labels.float())
-                loss.requires_grad_(True)
-                loss.backward()
-                optimizer.step()
-                if total_batch % 10 == 0:
-                    # 每多少轮输出在训练集和验证集上的效果
-                    true = labels.data.cpu()
-                    #print(true.float())
-                    predic = outputs
-                    predic[predic>=config.threshold] = 1.0
-                    predic[predic<config.threshold] = 0.0
-                    train_f1 = f1_score(np.array(true.float()), predic.cpu().detach().numpy(),average='weighted')
-                    dev_f1, dev_loss = evaluate(config, model, dev_iter)
-                    if dev_loss < dev_best_loss:
-                        dev_best_loss = dev_loss
-                        torch.save(model.state_dict(), config.save_path)
-                        improve = '*'
-                        last_improve = total_batch
-                    else:
-                        improve = ''
-                    train_time_dif = get_time_dif(start_time)
-                    msg = 'Iter: {0:>6},  Train Loss: {1:>5.2},  Train F1: {2:>6.2%},  Val Loss: {3:>5.2},  Val F1: {4:>6.2%},  Time: {5} {6}'
-                    print(msg.format(total_batch, loss.item(), train_f1, dev_loss, dev_f1, train_time_dif, improve))
-                    model.train()
-                total_batch += 1
-                if total_batch - last_improve > config.require_improvement:
-                    # 验证集loss超过1000batch没下降，结束训练
-                    print("No optimization for a long time, auto-stopping...")
-                    flag = True
-                    break
-            if flag:
+    #with torch.no_grad():
+    for epoch in range(config.num_epochs):
+        print('Epoch [{}/{}]'.format(epoch + 1, config.num_epochs))
+        for i, (trains, labels) in enumerate(train_iter):
+            outputs = model(trains)
+            outputs = torch.sigmoid(outputs)
+            model.zero_grad()
+            loss = nn.BCELoss()(outputs, labels.float())
+            loss.requires_grad_(True)
+            loss.backward()
+            optimizer.step()
+            if total_batch % 10 == 0:
+                # 每多少轮输出在训练集和验证集上的效果
+                true = labels.data.cpu()
+                #print(true.float())
+                predic = outputs
+                predic[predic>=config.threshold] = 1.0
+                predic[predic<config.threshold] = 0.0
+                train_f1 = f1_score(np.array(true.float()), predic.cpu().detach().numpy(),average='weighted')
+                dev_f1, dev_loss = evaluate(config, model, dev_iter)
+                if dev_loss < dev_best_loss:
+                    dev_best_loss = dev_loss
+                    torch.save(model.state_dict(), config.save_path)
+                    improve = '*'
+                    last_improve = total_batch
+                else:
+                    improve = ''
+                train_time_dif = get_time_dif(start_time)
+                msg = 'Iter: {0:>6},  Train Loss: {1:>5.2},  Train F1: {2:>6.2%},  Val Loss: {3:>5.2},  Val F1: {4:>6.2%},  Time: {5} {6}'
+                print(msg.format(total_batch, loss.item(), train_f1, dev_loss, dev_f1, train_time_dif, improve))
+                model.train()
+            total_batch += 1
+            if total_batch - last_improve > config.require_improvement:
+                # 验证集loss超过1000batch没下降，结束训练
+                print("No optimization for a long time, auto-stopping...")
+                flag = True
                 break
+        if flag:
+            break
     test(config, model, model_name, train_time_dif, test_iter)
 
 
